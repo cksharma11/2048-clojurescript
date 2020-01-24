3193df26981f08d5ba976102ebba9afc751b37a9
Chandan Kumar <cksharma122@gmail.com>
2020-01-24 14:49:42 +0530

    Fix new cell generation

--- src/two048/core.cljs
+++ src/two048/core.cljs
@@ -1,29 +1,29 @@
 (ns two048.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [two048.logic :as l]))
+	(:require [reagent.core :as reagent :refer [atom]]
+						[two048.logic :as l]))
 
 (enable-console-print!)
 
 (defonce state (reagent/atom '((0 0 0 0)
-                               (2 2 0 0)
-                               (4 0 0 2)
-                               (0 0 0 0))))
+															 (2 2 0 0)
+															 (4 0 0 2)
+															 (0 0 0 0))))
 
 (defn create-cell [cell]
-  [:div {:class (str "cell _" cell)} (str cell)])
+	[:div {:class (str "cell _" cell)} (str cell)])
 
 (defn create-row [row]
-  [:div {:class "row"}
-   (map create-cell row)])
+	[:div {:class "row"}
+	 (map create-cell row)])
 
 (defn board []
-  [:div {:class "board" :tabindex -1 :on-key-down #(case (.-which %)
-                                                     38 (swap! state (comp l/move-board-up l/place-new-cell))
-                                                     40 (swap! state l/move-board-down)
-                                                     37 (swap! state l/move-board-left)
-                                                     39 (swap! state l/move-board-right)
-                                                     nil)}
-   (map create-row @state)])
+	[:div {:class "board" :tabindex -1 :on-key-down #(case (.-which %)
+																										 38 (swap! state (comp l/move-board-up l/place-new-cell))
+																										 40 (swap! state (comp l/move-board-down l/place-new-cell))
+																										 37 (swap! state (comp l/move-board-left l/place-new-cell))
+																										 39 (swap! state (comp l/move-board-right l/place-new-cell))
+																										 nil)}
+	 (map create-row @state)])
 
 (reagent/render-component [board]
-                          (. js/document (getElementById "app")))
+													(. js/document (getElementById "app")))
