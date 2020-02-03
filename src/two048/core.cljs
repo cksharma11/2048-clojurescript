cb75ba6fbcdac81e7ecac1222ab581c2715d1ba0
Chandan Kumar <cksharma122@gmail.com>
2020-02-03 23:04:30 +0530

    Fix indentation

--- src/two048/core.cljs
+++ src/two048/core.cljs
@@ -1,34 +1,34 @@
 (ns two048.core
-	(:require [reagent.core :as reagent :refer [atom]]
-						[two048.logic :as l]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [two048.logic :as l]))
 
 (enable-console-print!)
 
 (defonce state (reagent/atom '((0 0 0 0)
-															 (2 2 0 0)
-															 (4 0 0 2)
-															 (0 0 0 0))))
+                               (2 2 0 0)
+                               (4 0 0 2)
+                               (0 0 0 0))))
 
 (defn get-cell-representation [cell]
-	(str (if (= 0 cell) "" cell)))
+  (str (if (= 0 cell) "" cell)))
 
 (defn create-cell [cell]
-	[:div {:class (str "cell _" cell)} (get-cell-representation cell)])
+  [:div {:class (str "cell _" cell)} (get-cell-representation cell)])
 
 (defn create-row [row]
-	[:div {:class "row"}
-	 (map create-cell row)])
+  [:div {:class "row"}
+   (map create-cell row)])
 
 (defn board []
-	[:div {:class "board" :tabindex -1 :on-key-down
-								#(case (.-which %)
-									 38 (swap! state (comp l/place-new-cell l/move-board-up))
-									 40 (swap! state (comp l/place-new-cell l/move-board-down))
-									 37 (swap! state (comp l/place-new-cell l/move-board-left))
-									 39 (swap! state (comp l/place-new-cell l/move-board-right))
-									 nil)}
+  [:div {:class "board" :tabindex -1 :on-key-down
+         #(case (.-which %)
+            38 (swap! state (comp l/place-new-cell l/move-board-up))
+            40 (swap! state (comp l/place-new-cell l/move-board-down))
+            37 (swap! state (comp l/place-new-cell l/move-board-left))
+            39 (swap! state (comp l/place-new-cell l/move-board-right))
+            nil)}
 
-	 (map create-row @state)])
+   (map create-row @state)])
 
 (reagent/render-component [board]
-													(. js/document (getElementById "app")))
+                          (. js/document (getElementById "app")))
--- src/two048/logic.cljs
+++ src/two048/logic.cljs
@@ -8,31 +8,31 @@
 (def remove-zeroes (partial filter pos?))
 
 (defn append-zeroes [size coll]
-			(take size (concat coll (repeat 0))))
+  (take size (concat coll (repeat 0))))
 
 (defn create-new-cell [board]
-			(loop [row (get-random)
-						 col (get-random)]
-						(if (= 0 (get-in (mapv vec board) [row col]))
-							[row col]
-							(recur (get-random) (get-random)))))
+  (loop [row (get-random)
+         col (get-random)]
+    (if (= 0 (get-in (mapv vec board) [row col]))
+      [row col]
+      (recur (get-random) (get-random)))))
 
 (defn place-new-cell [board]
-			(map (partial apply list)
-					 (assoc-in (mapv vec board)
-										 (create-new-cell board) (get-new-cell))))
+  (map (partial apply list)
+       (assoc-in (mapv vec board)
+                 (create-new-cell board) (get-new-cell))))
 
 (def move-left-row-without-zeroes
-	(comp
-		(partial map (partial apply +))
-		(partial mapcat (partial partition-all 2))
-		(partial partition-by identity)
-		(partial remove-zeroes)))
+  (comp
+   (partial map (partial apply +))
+   (partial mapcat (partial partition-all 2))
+   (partial partition-by identity)
+   (partial remove-zeroes)))
 
 (def move-left-row
-	(comp
-		(partial apply append-zeroes)
-		(juxt count move-left-row-without-zeroes)))
+  (comp
+   (partial apply append-zeroes)
+   (juxt count move-left-row-without-zeroes)))
 
 (def move-board-left (partial map move-left-row))
 
