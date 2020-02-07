99d337338519c952ce97216b5f291537e7746b93
Chandan Kumar <cksharma122@gmail.com>
2020-02-07 15:13:52 +0530

    Refactor game over logic

--- src/two048/core.cljs
+++ src/two048/core.cljs
@@ -1,66 +1,66 @@
 (ns two048.core
- (:require [reagent.core :as reagent :refer [atom]]
-					 [two048.logic :as l]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [two048.logic :as l]))
 
 (enable-console-print!)
 
 (def initial-state {:board '((0 0 0 0)
-														 (0 2 2 0)
-														 (0 0 0 2)
-														 (0 0 0 0))
-										:over  false})
+                             (0 2 2 0)
+                             (0 0 0 2)
+                             (0 0 0 0))
+                    :over  false})
 
 (defonce state (reagent/atom initial-state))
 
 (defn update-state [board]
- (if (l/game-over? board)
-	{:board board :over true}
-	{:board (l/place-new-cell board) :over false}))
+  (if (l/game-over? board)
+    {:board board :over true}
+    {:board (l/place-new-cell board) :over false}))
 
 (defn move-up [state]
- (update-state (l/move-board-up (:board state))))
+  (update-state (l/move-board-up (:board state))))
 
 (defn move-down [state]
- (update-state (l/move-board-down (:board state))))
+  (update-state (l/move-board-down (:board state))))
 
 (defn move-left [state]
- (update-state (l/move-board-left (:board state))))
+  (update-state (l/move-board-left (:board state))))
 
 (defn move-right [state]
- (update-state (l/move-board-right (:board state))))
+  (update-state (l/move-board-right (:board state))))
 
 (defn reset-game []
- (reset! state initial-state ))
+  (reset! state initial-state))
 
 (defn get-cell-representation [cell]
- (str (if (= 0 cell) "" cell)))
+  (str (if (= 0 cell) "" cell)))
 
 (defn create-cell [cell]
- [:div {:class (str "cell _" cell)} (get-cell-representation cell)])
+  [:div {:class (str "cell _" cell)} (get-cell-representation cell)])
 
 (defn create-row [row]
- [:div {:class "row"}
-	(map create-cell row)])
+  [:div {:class "row"}
+   (map create-cell row)])
 
 (defn game-over-view [state]
- (if (:over state)
-	[:div {:class "game-over"}
-	 [:div "Game over"]
-	 [:button {:class "play-again" :on-click reset-game} "Play again"]]
-	[:div]))
+  (if (:over state)
+    [:div {:class "game-over"}
+     [:div "Game over"]
+     [:button {:class "play-again" :on-click reset-game} "Play again"]]
+    [:div]))
 
 (defn board []
- [:div {:class "container"}
-	[:div {:class "board" :autofocus 1 :tabindex 1 :on-key-down
-								#(case (.-which %)
-									38 (swap! state move-up)
-									40 (swap! state move-down)
-									37 (swap! state move-left)
-									39 (swap! state move-right)
-									nil)}
+  [:div {:class "container"}
+   [:div {:class "board" :autofocus 1 :tabindex 1 :on-key-down
+          #(case (.-which %)
+             38 (swap! state move-up)
+             40 (swap! state move-down)
+             37 (swap! state move-left)
+             39 (swap! state move-right)
+             nil)}
 
-	 (map create-row (:board @state))]
-	(game-over-view @state)])
+    (map create-row (:board @state))]
+   (game-over-view @state)])
 
 (reagent/render-component [board]
-													(. js/document (getElementById "app")))
+                          (. js/document (getElementById "app")))
--- src/two048/logic.cljs
+++ src/two048/logic.cljs
@@ -8,36 +8,36 @@
 (def remove-zeroes (partial filter pos?))
 
 (defn append-zeroes [size coll]
- (take size (concat coll (repeat 0))))
+  (take size (concat coll (repeat 0))))
 
 (defn map-2d-index [pos]
- [(quot pos board-size) (mod pos board-size)])
+  [(quot pos board-size) (mod pos board-size)])
 
 (defn get-empty-positions [board]
- (map map-2d-index (keep-indexed (fn [k v] (when (zero? v) k)) (flatten board))))
+  (map map-2d-index (keep-indexed (fn [k v] (when (zero? v) k)) (flatten board))))
 
 (defn create-new-cell [board]
- (let [empty-cells (get-empty-positions board)]
-	(if (zero? (count empty-cells))
-	 [-1 -1]
-	 (rand-nth empty-cells))))
+  (let [empty-cells (get-empty-positions board)]
+    (if (zero? (count empty-cells))
+      [-1 -1]
+      (rand-nth empty-cells))))
 
 (defn place-new-cell [board]
- (map (partial apply list)
-			(assoc-in (mapv vec board)
-								(create-new-cell board) (get-new-cell))))
+  (map (partial apply list)
+       (assoc-in (mapv vec board)
+                 (create-new-cell board) (get-new-cell))))
 
 (def move-left-row-without-zeroes
- (comp
-	(partial map (partial apply +))
-	(partial mapcat (partial partition-all 2))
-	(partial partition-by identity)
-	(partial remove-zeroes)))
+  (comp
+   (partial map (partial apply +))
+   (partial mapcat (partial partition-all 2))
+   (partial partition-by identity)
+   (partial remove-zeroes)))
 
 (def move-left-row
- (comp
-	(partial apply append-zeroes)
-	(juxt count move-left-row-without-zeroes)))
+  (comp
+   (partial apply append-zeroes)
+   (juxt count move-left-row-without-zeroes)))
 
 (def move-board-left (partial map move-left-row))
 
@@ -49,14 +49,13 @@
 
 (def move-board-down (comp transpose move-board-right transpose))
 
-(defn any-cell-not-empty? [board]
- (not (some zero? (flatten board))))
+(defn not-empty-cell? [board]
+  (not (some zero? (flatten board))))
 
-(defn any-adjacent-not-same? [coll]
- (= (count coll)
-		(count (dedupe coll))))
+(defn no-same-adjacent [coll]
+  (= coll (dedupe coll)))
 
 (defn game-over? [board]
- (and (every? any-adjacent-not-same? board)
-			(every? any-adjacent-not-same? (transpose board))
-			(any-cell-not-empty? board)))
+  (and (every? no-same-adjacent board)
+       (every? no-same-adjacent (transpose board))
+       (not-empty-cell? board)))
